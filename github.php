91accb0ba2f890cfcfc05a07afad267df6aae3e1
Arian cedillo <acedilloc@ascm.gob.mx>
2016-05-16 17:59:40 -0500

    2ac

--- github.php
+++ github.php
@@ -1,3 +1,5 @@
 <?php
-exec("C:/Program Files/Git/usr/ binpull 2>&1", $out);
-var_dump($out);
+$out ="star C:\Progra~1/Git/usr/bin pull 2>&1"
+//exec("C:/Program Files/Git/usr/ binpull 2>&1", $out);
+//var_dump($out);
+exec ($out);
