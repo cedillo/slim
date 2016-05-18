c6724ffd2afd01541d8d4cfa5c83b39b53d5e794
Arian cedillo <acedilloc@ascm.gob.mx>
2016-05-18 13:15:43 -0500

    lista auditores

--- index.php
+++ index.php
@@ -577,6 +577,20 @@ $app->post('/guardar/papel', function()  use($app, $db) {
 	 ***********************************************************************************
 	*/
 
+//lista de auditores
+ 		$app->get('/lstAuditoresByID/:id', function($id)    use($app, $db) {
+ 			$sql="SELECT CONCAT(au.nombre,' ',au.paterno,' ',au.materno) nombre, p.nombre puesto " .
+ 				"FROM sia_auditores au LEFT JOIN sia_plazas p on au.idPlaza = p.idPlaza where au.idArea='DGACFA'";
+ 			$dbQuery = $db->prepare($sql);
+ 			$dbQuery->execute(array(':id' => $id));
+ 			$result = $dbQuery->fetch(PDO::FETCH_ASSOC);
+ 			if(!$result){
+ 				$app->halt(404, "NO SE ENCONTRARON DATOS ");
+ 			}else{
+ 				echo json_encode($result);
+ 			}
+ 		});
+
 	//Guarda un avanceActividad
 $app->post('/guardar/avance', function()  use($app, $db) {
 		$usrActual = $_SESSION["idUsuario"];
