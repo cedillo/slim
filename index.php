2b7070920e46f8639aac4b7fc55a5c9522ded684
jacotaz <josecota@gmail.com>
2016-05-19 12:06:43 -0500

    Test#2

--- index.php
+++ index.php
@@ -406,7 +406,50 @@
 	});	
 
 	
+	$app->get('/cargarArchivo/Ingreso/:nombre/:cuenta', function($nombre, $cuenta)    use($app, $db) {	
+		try{
+			$usrActual = $_SESSION["idUsuario"];
+			$archivo ='uploads/' . $nombre;
 
+			//Elimina cuenta pública
+			$sql="DELETE FROM sia_cuentasingresos WHERE idCuenta= :cuenta ;";
+			$dbQuery = $db->prepare($sql);
+			$dbQuery->execute(array(':cuenta' => $cuenta));
+
+			//Abrir archivo de XLS
+			$xlsx = new SimpleXLSX($archivo);
+			list($num_cols, $num_rows) = $xlsx->dimension();			
+
+			//insertar 
+			$sql="INSERT INTO sia_cuentasingresos (idCuenta, origen, tipo, clave, nivel, nombre,  original, recaudado, usrAlta, fAlta, estatus) " .
+			"values(:cuenta,:origen, :tipo, :clave, :nivel, :nombre, :original, :recaudado, :usrActual, getdate(), 'ACTIVO')";
+
+			$dbQuery = $db->prepare($sql);
+			$nRegistros=0;
+			
+			error_reporting(E_ALL ^ E_NOTICE);
+			
+			foreach( $xlsx->rows() as $row ) {				
+				$origen = $row[0];
+				$tipo =  "" . $row[1];
+				$clave =  "" . $row[2];
+				$nivel =  "" . $row[3];
+				$nombre =  "" . $row[4];
+				$original =  "" . $row[5];
+				$recaudado =  "" . $row[6];
+								
+				if ($nRegistros>0){
+					$dbQuery->execute(array(':cuenta' => $cuenta, ':origen' => $origen, ':tipo' => $tipo,':clave' => $clave, ':nivel' => $nivel, ':nombre' => $nombre, 
+					':original' => $original, ':recaudado' => $recaudado,':usrActual' => $usrActual));								
+				}				
+				$nRegistros++;	
+			}
+			echo "Se cargaron " . $nRegistros . " registro(s).";
+		}catch (PDOException $e) {
+			echo  "<br>¡Error en el TRY!: " . $e->getMessage();
+			die();
+		}
+	});
 
 	$app->get('/cargarArchivo/Egreso/:nombre/:cuenta', function($nombre, $cuenta)    use($app, $db) {	
 		try{
